04fb959c9929d3d97bcc80eb5710fd771e34096f
Carlos Hernández <carlossanchez25fasa@gmail.com>
2025-11-22 21:58:22 -0500

    fix: ajustes despliegue app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/rincones_boyaca/",
@@ -10,10 +9,12 @@ export default defineConfig({
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          mui: ["@mui/material", "@emotion/react", "@emotion/styled"],
-          leaflet: ["leaflet", "react-leaflet"]
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("@mui")) return "mui";
+            if (id.includes("leaflet") || id.includes("react-leaflet")) return "leaflet";
+            if (id.includes("react")) return "react";
+          }
         }
       }
     }
