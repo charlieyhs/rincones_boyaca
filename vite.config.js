0aebac25e2182bc95524a3cf69548ab59a3c397c
Carlos Hernández <carlossanchez25fasa@gmail.com>
2025-11-22 21:53:33 -0500

    fix: Ajuste configuración despliegue aplicación

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/rincones_boyaca/",
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          mui: ["@mui/material", "@emotion/react", "@emotion/styled"],
+          leaflet: ["leaflet", "react-leaflet"]
+        }
+      }
+    }
   }
 });
